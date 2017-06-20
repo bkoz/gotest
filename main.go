27695a6ffda092ab885a962c77bb8ed9b0498c5b
Bob Kozdemba <bkozdemba@gmail.com>
2017-06-20 17:51:39 -0500

    Update main.go
    changed image size

--- main.go
+++ main.go
@@ -49,8 +49,8 @@ func redHandler(w http.ResponseWriter, r *http.Request) {
 
 // plotHandler - Draw the fractal image.
 func fracHandler(w http.ResponseWriter, r *http.Request) {
-	width := 512
-	height := 512
+	width := 64
+	height := 64
 	m := image.NewRGBA(image.Rect(0, 0, width, height))
 	mandel := createImage(width, height)
 	draw.Draw(m, m.Bounds(), mandel, image.ZP, draw.Src)
