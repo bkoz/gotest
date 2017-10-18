f0304b7fb90371f5765372fef5cbd52543b284e8
Bob Kozdemba <bkozdemba@gmail.com>
2017-10-17 20:36:45 -0400

    added random contrast

--- main.go
+++ main.go
@@ -53,9 +53,7 @@ func redHandler(w http.ResponseWriter, r *http.Request) {
 // plotHandler - Draw the fractal image.
 func fracHandler(w http.ResponseWriter, r *http.Request) {
 	rand.Seed(time.Now().UTC().UnixNano())
-	log.Println("fracHandler running ", rand.Intn(1000))
-	// global
-	randNumber = rand.Intn(1000)
+	log.Println("fracHandler running ", rand.Intn(15))
 	width := 64
 	height := 64
 	m := image.NewRGBA(image.Rect(0, 0, width, height))
@@ -137,6 +135,8 @@ func mandelbrot(z complex128) color.Color {
 	const iterations = 200
 	const contrast = 15
 
+	var randNumber = uint8(rand.Intn(15))
+
 	var v complex128
 
 	lut := make(map[uint8]color.Color)
@@ -154,7 +154,7 @@ func mandelbrot(z complex128) color.Color {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
 			// return color.Gray{255 - contrast*n}
-			return palette.Plan9[255-contrast*n]
+			return palette.Plan9[255-randNumber*n]
 			// return lut[255-contrast*n]
 		}
 	}
